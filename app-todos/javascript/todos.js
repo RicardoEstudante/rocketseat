b3fa5dad9d7f14429c4c0dcd6437e7e8bc42ede1
Ricardo Carvalho <ricardo2010carvalho@hotmail.com>
2020-03-09 17:37:33 -0300

    upando o projeto de listas adds pela JS

--- app-todos/javascript/todos.js
+++ app-todos/javascript/todos.js
@@ -1,25 +1,58 @@
+    // salvando cada elemento dentro de uma variavel (ul, input, button)
 var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Fazer café',
-    'Estudar Javascript',
-    'Acessar comunidade da Rockeseat'
-];
+var todos = JSON.parse(localStorage.getItem('List_todos')) || [];
 
+    // função para renderizar a lista na tela
 function renderTodos() {
+    listElement.innerHTML = '';
+
     for (todo of todos) {
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
 
+        var linkElement = document.createElement('a');
+
+        linkElement.setAttribute('href', '#');
+
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')')
+
+        var linkText = document.createTextNode('excluir');
+
+        linkElement.appendChild(linkText);
+
         todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
         listElement.appendChild(todoElement);
     }
 }
 
 renderTodos();
 
+    // função para adicionar membros a lista
 function addTodo() {
     var todoText = inputElement.value;
+
+    todos.push(todoText);
+    inputElement.value = '';
+    renderTodos();
+    saveToStorage();
 }
+
+buttonElement.onclick = addTodo;
+
+    // função para deletar list
+function deleteTodo(pos) {
+    todos.splice(pos, 1);
+    renderTodos();
+    saveToStorage();
+}
+
+    // função para salvar listas no cache do navegador
+function saveToStorage() {
+    localStorage.setItem('List_todos', JSON.stringify(todos));
+}
+    
