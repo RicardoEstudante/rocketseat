ca36f86ce2be3013b180d22c68ed17e9f7f095be
Ricardo Carvalho <ricardo2010carvalho@hotmail.com>
2020-03-16 16:02:40 -0300

    att

---
+++ javascript-es6/learn/spread/spread.js
@@ -0,0 +1,17 @@
+//SPREAD
+
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+
+const arr3 = [ ...arr1, ...arr2];
+
+console.log(arr3);
+
+const user = {
+    nome: 'Ricardo',
+    age: 22,
+    company: '55pbx'
+};
+
+const userTwo = { ...user, nome: 'Ricardo Carvalho' };
+console.log(userTwo);
